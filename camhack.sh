7f629c66e53f99b2fa78f9ee3dde3908996b5d1c
Cyber Tonian <65481132+CyberTonia@users.noreply.github.com>
2020-10-21 09:22:08 +0530

    Update camhack.sh

--- camhack.sh
+++ camhack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CamHack v2.0
-# Powered by MuqeeVirus [GEAR]
+# Powered by Cyber Tonian
 # Credits goes to thelinuxchoice [github.com/thelinuxchoice/]
 
 trap 'printf "\n";stop' 2
@@ -16,7 +16,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m | (   ) | | (    ) | |
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m | )   ( | | )    ( | | (____/\  | ) \ \  \e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m |/     \| |/      \| (_______/  |/   \_\ ver 2.0 \e[0m\n\n"
                                                                                                 
-printf " \e[1;77m  youtube.com/MuqeeVirus | grmusa9797@gmail.com \e[0m \n"
+printf " \e[1;77m  youtube.com/CyberTonian | grmusa9797@gmail.com \e[0m \n"
 
 printf "\n"
 
